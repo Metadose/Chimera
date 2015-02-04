84be30951019c35924e76c537d9ec0a27a5e209e
unknown <cebedo.vii@gmail.com>
2015-02-04 12:00:44 +0800

    controller update

--- src/main/java/com/cebedo/pms/project/controller/ProjectController.java
+++ src/main/java/com/cebedo/pms/project/controller/ProjectController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("project")
 public class ProjectController {
 
+	@RequestMapping(value = { "/list" })
+	public String getProjectList() {
+		return "index";
+	}
+
 	@RequestMapping(value = { "/index", "wow" })
 	public String index() {
 		return "index";
