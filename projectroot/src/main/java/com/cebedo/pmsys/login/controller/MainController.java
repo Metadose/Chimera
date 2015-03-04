2485764d31e7ed40a97eb352904c860dd27ce2a1
VicCebedo <cebedo.vii@gmail.com>
2015-03-04 15:54:30 +0800

    progress on security, moving back to krams

--- projectroot/src/main/java/com/cebedo/pmsys/login/controller/MainController.java
+++ projectroot/src/main/java/com/cebedo/pmsys/login/controller/MainController.java
@@ -1,82 +1,52 @@
 package com.cebedo.pmsys.login.controller;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * Handles and retrieves the common or admin page depending on the URI template.
+ * A user must be log-in first he can access these pages. Only the admin can see
+ * the adminpage, however.
+ */
 @Controller
+@RequestMapping("/main")
 public class MainController {
 
-	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
-	public ModelAndView defaultPage() {
+	protected static Logger logger = Logger.getLogger("controller");
 
-		ModelAndView model = new ModelAndView();
-		model.addObject("title",
-				"Spring Security Login Form - Database Authentication");
-		model.addObject("message", "This is default page!");
-		model.setViewName("hello");
-		return model;
+	/**
+	 * Handles and retrieves the common JSP page that everyone can see
+	 * 
+	 * @return the name of the JSP page
+	 */
+	@RequestMapping(value = "/common", method = RequestMethod.GET)
+	public String getCommonPage() {
+		logger.debug("Received request to show common page");
 
+		// Do your work here. Whatever you like
+		// i.e call a custom service to do your business
+		// Prepare a model to be used by the JSP page
+
+		// This will resolve to /WEB-INF/jsp/commonpage.jsp
+		return "commonpage";
 	}
 
-	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
-	public ModelAndView adminPage() {
+	/**
+	 * Handles and retrieves the admin JSP page that only admins can see
+	 * 
+	 * @return the name of the JSP page
+	 */
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String getAdminPage() {
+		logger.debug("Received request to show admin page");
 
-		ModelAndView model = new ModelAndView();
-		model.addObject("title",
-				"Spring Security Login Form - Database Authentication");
-		model.addObject("message", "This page is for ROLE_ADMIN only!");
-		model.setViewName("admin");
+		// Do your work here. Whatever you like
+		// i.e call a custom service to do your business
+		// Prepare a model to be used by the JSP page
 
-		return model;
-
+		// This will resolve to /WEB-INF/jsp/adminpage.jsp
+		return "adminpage";
 	}
-
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login(
-			@RequestParam(value = "error", required = false) String error,
-			@RequestParam(value = "logout", required = false) String logout) {
-
-		ModelAndView model = new ModelAndView();
-		if (error != null) {
-			model.addObject("error", "Invalid username and password!");
-		}
-
-		if (logout != null) {
-			model.addObject("msg", "You've been logged out successfully.");
-		}
-		model.setViewName("login");
-
-		return model;
-
-	}
-
-	// for 403 access denied page
-	@RequestMapping(value = "/403", method = RequestMethod.GET)
-	public ModelAndView accesssDenied() {
-
-		ModelAndView model = new ModelAndView();
-
-		// check if user is login
-		Authentication auth = SecurityContextHolder.getContext()
-				.getAuthentication();
-		if (!(auth instanceof AnonymousAuthenticationToken)) {
-			UserDetails userDetail = (UserDetails) auth.getPrincipal();
-			System.out.println(userDetail);
-
-			model.addObject("username", userDetail.getUsername());
-
-		}
-
-		model.setViewName("403");
-		return model;
-
-	}
-
 }
