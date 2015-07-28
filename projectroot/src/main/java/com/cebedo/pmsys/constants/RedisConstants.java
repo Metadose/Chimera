c452039af8caf26f96fe8a21cc4dd3fd43a939fa
Vic Cebedo <cebedo.vii@gmail.com>
2015-07-28 16:46:41 +0800

    more diet stuff

--- projectroot/src/main/java/com/cebedo/pmsys/constants/RedisConstants.java
+++ projectroot/src/main/java/com/cebedo/pmsys/constants/RedisConstants.java
@@ -19,11 +19,6 @@ public abstract class RedisConstants {
     public static final String OBJECT_UNIT = "unit";
     public static final String OBJECT_PLASTER_MIXTURE = "plastermixture";
 
-    public static final String OBJECT_CHB = "chb";
-    public static final String OBJECT_CHB_FOOTING_DIMENSION = "chbfootingdimension";
-    public static final String OBJECT_CHB_FOOTING_DIMENSION_DISPLAY = "CHB footing dimension";
-    public static final String OBJECT_CHB_FOOTING_MIXTURE = "chbfootingmixture";
-    public static final String OBJECT_CHB_FOOTING_MIXTURE_DISPLAY = "CHB footing mixture";
     public static final String OBJECT_CHB_HORIZONTAL_REINFORCEMENT = "chbhorizontalreinforcement";
     public static final String OBJECT_CHB_HORIZONTAL_REINFORCEMENT_DISPLAY = "CHB horizontal reinforcement";
     public static final String OBJECT_CHB_VERTICAL_REINFORCEMENT = "chbverticalreinforcement";
@@ -32,16 +27,12 @@ public abstract class RedisConstants {
     public static final String OBJECT_MASONRY_CHB_ESTIMATION_SUMMARY = "masonrychbestimationsummary";
     public static final String OBJECT_MASONRY_CHB_ESTIMATION_SUMMARY_DISPLAY = "masonry (CHB) estimation summary";
 
-    public static final String OBJECT_BLOCK_LAYING_MIXTURE = "blocklayingmixture";
-    public static final String OBJECT_BLOCK_LAYING_MIXTURE_DISPLAY = "block laying mixture";
     public static final String OBJECT_ESTIMATION_ALLOWANCE = "estimationallowance";
     public static final String OBJECT_ESTIMATION_ALLOWANCE_DISPLAY = "estimation allowance";
     public static final String OBJECT_MATERIAL_CATEGORY = "materialcategory";
 
     public static final String OBJECT_CONCRETE_ESTIMATION_SUMMARY = "concreteestimationsummary";
     public static final String OBJECT_CONCRETE_ESTIMATION_SUMMARY_DISPLAY = "concrete estimation summary";
-    public static final String OBJECT_CONCRETE_PROPORTION = "concreteproportion";
-    public static final String OBJECT_CONCRETE_PROPORTION_DISPLAY = "concrete proportion";
     public static final String OBJECT_MATERIAL_CATEGORY_DISPLAY = "material category";
 
     /**
@@ -59,16 +50,10 @@ public abstract class RedisConstants {
 	    + "/materialPullout";
     public static final String JSP_ATTENDANCE_LIST = OBJECT_ATTENDANCE
 	    + "/attendanceList";
-    public static final String JSP_CHB_LIST = OBJECT_CHB + "/chbList";
-    public static final String JSP_CHB_EDIT = OBJECT_CHB + "/chbEdit";
     public static final String JSP_PLASTER_MIXTURE_LIST = OBJECT_PLASTER_MIXTURE
 	    + "/plasterMixtureList";
     public static final String JSP_PLASTER_MIXTURE_EDIT = OBJECT_PLASTER_MIXTURE
 	    + "/plasterMixtureEdit";
-    public static final String JSP_BLOCK_LAYING_MIXTURE_LIST = OBJECT_BLOCK_LAYING_MIXTURE
-	    + "/blockLayingMixtureList";
-    public static final String JSP_BLOCK_LAYING_MIXTURE_EDIT = OBJECT_BLOCK_LAYING_MIXTURE
-	    + "/blockLayingMixtureEdit";
     public static final String JSP_UNIT_LIST = OBJECT_UNIT + "/unitList";
     public static final String JSP_UNIT_EDIT = OBJECT_UNIT + "/unitEdit";
     public static final String JSP_CHB_HORIZONTAL_REINFORCEMENT_LIST = OBJECT_CHB_HORIZONTAL_REINFORCEMENT
@@ -79,14 +64,6 @@ public abstract class RedisConstants {
 	    + "/chbVerticalReinforcementList";
     public static final String JSP_CHB_VERTICAL_REINFORCEMENT_EDIT = OBJECT_CHB_VERTICAL_REINFORCEMENT
 	    + "/chbVerticalReinforcementEdit";
-    public static final String JSP_CHB_FOOTING_DIMENSION_LIST = OBJECT_CHB_FOOTING_DIMENSION
-	    + "/chbFootingDimensionList";
-    public static final String JSP_CHB_FOOTING_DIMENSION_EDIT = OBJECT_CHB_FOOTING_DIMENSION
-	    + "/chbFootingDimensionEdit";
-    public static final String JSP_CHB_FOOTING_MIXTURE_LIST = OBJECT_CHB_FOOTING_MIXTURE
-	    + "/chbFootingMixtureList";
-    public static final String JSP_CHB_FOOTING_MIXTURE_EDIT = OBJECT_CHB_FOOTING_MIXTURE
-	    + "/chbFootingMixtureEdit";
 
     public static final String JSP_ESTIMATION_ALLOWANCE_LIST = OBJECT_ESTIMATION_ALLOWANCE
 	    + "/estimationAllowanceList";
@@ -98,8 +75,4 @@ public abstract class RedisConstants {
 	    + "/materialCategoryList";
     public static final String JSP_MATERIAL_CATEGORY_EDIT = OBJECT_MATERIAL_CATEGORY
 	    + "/materialCategoryEdit";
-    public static final String JSP_CONCRETE_PROPORTION_LIST = OBJECT_CONCRETE_PROPORTION
-	    + "/concreteProportionList";
-    public static final String JSP_CONCRETE_PROPORTION_EDIT = OBJECT_CONCRETE_PROPORTION
-	    + "/concreteProportionEdit";
 }
