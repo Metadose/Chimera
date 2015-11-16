9fbe2ffa0ab451aa501b6ecb83aadfd885c274e6
VicCebedo <cebedo.vii@gmail.com>
2015-11-16 09:46:37 +0800

    Finalizing release.

--- mod-webapp-db/2015.11.16.Workbench.sql
+++ mod-webapp-db/2015.11.16.Workbench.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `test`;
 -- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
 --
 -- Host: localhost    Database: test
@@ -35,16 +37,6 @@ CREATE TABLE `assignments_project_field` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `assignments_project_field`
---
-
-LOCK TABLES `assignments_project_field` WRITE;
-/*!40000 ALTER TABLE `assignments_project_field` DISABLE KEYS */;
-INSERT INTO `assignments_project_field` VALUES (32,1,'qweqweqwe2222vvvqq','qweqweqwewqeqwe');
-/*!40000 ALTER TABLE `assignments_project_field` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `assignments_project_staff`
 --
 
@@ -62,16 +54,6 @@ CREATE TABLE `assignments_project_staff` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `assignments_project_staff`
---
-
-LOCK TABLES `assignments_project_staff` WRITE;
-/*!40000 ALTER TABLE `assignments_project_staff` DISABLE KEYS */;
-INSERT INTO `assignments_project_staff` VALUES (32,74),(32,75),(32,77),(32,78),(32,79);
-/*!40000 ALTER TABLE `assignments_project_staff` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `assignments_task_staff`
 --
 
@@ -87,15 +69,6 @@ CREATE TABLE `assignments_task_staff` (
   CONSTRAINT `FK_tasks_task` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `assignments_task_staff`
---
-
-LOCK TABLES `assignments_task_staff` WRITE;
-/*!40000 ALTER TABLE `assignments_task_staff` DISABLE KEYS */;
-/*!40000 ALTER TABLE `assignments_task_staff` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `audit_logs`
@@ -124,20 +97,9 @@ CREATE TABLE `audit_logs` (
   KEY `FK_audit_logs_project_id` (`project_id`),
   KEY `FK_audit_logs_company_id` (`company_id`),
   CONSTRAINT `FK_audit_logs_company_id` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`),
-  CONSTRAINT `FK_audit_logs_project_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`),
-  CONSTRAINT `FK_audit_logs_users` FOREIGN KEY (`user_id`) REFERENCES `system_users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+  CONSTRAINT `FK_audit_logs_project_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `audit_logs`
---
-
-LOCK TABLES `audit_logs` WRITE;
-/*!40000 ALTER TABLE `audit_logs` DISABLE KEYS */;
-INSERT INTO `audit_logs` VALUES (1,'2015-09-30 11:49:04','127.0.0.1',228,10,32,120,'project',32,NULL,'staff',NULL,'All','All'),(2,'2015-09-30 11:49:17','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',75,NULL,' Smith Johnson Williams '),(3,'2015-09-30 11:49:17','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',76,NULL,' Williams Smith Johnson '),(4,'2015-09-30 11:49:17','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',77,NULL,' Johnson Smith Williams '),(5,'2015-09-30 11:49:31','127.0.0.1',228,10,32,101,'project',32,NULL,'staff',NULL,'Mass','Mass'),(6,'2015-09-30 11:49:31','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',76,NULL,' Williams Smith Johnson '),(7,'2015-09-30 11:49:31','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',77,NULL,' Johnson Smith Williams '),(8,'2015-09-30 11:49:31','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',78,NULL,'Engr. Bob Marley Lannister '),(9,'2015-09-30 11:49:32','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',79,NULL,'Dr. Erlang Churva Looo III'),(10,'2015-09-30 11:49:32','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',74,NULL,' Vic Castillo Cebedo II'),(11,'2015-09-30 11:49:32','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',75,NULL,' Smith Johnson Williams '),(12,'2015-09-30 11:49:52','127.0.0.1',228,10,32,119,'project',32,NULL,'staff',76,NULL,' Williams Smith Johnson ');
-/*!40000 ALTER TABLE `audit_logs` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `companies`
@@ -153,19 +115,10 @@ CREATE TABLE `companies` (
   `date_started` datetime NOT NULL,
   `date_expiration` datetime NOT NULL,
   `beta_tester` tinyint(4) NOT NULL DEFAULT '0',
+  `theme` varchar(32) NOT NULL,
   PRIMARY KEY (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `companies`
---
-
-LOCK TABLES `companies` WRITE;
-/*!40000 ALTER TABLE `companies` DISABLE KEYS */;
-INSERT INTO `companies` VALUES (10,'Demo 1','','2015-09-01 00:00:00','2016-01-07 00:00:00',1);
-/*!40000 ALTER TABLE `companies` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `fields`
@@ -179,18 +132,8 @@ CREATE TABLE `fields` (
   `name` varchar(32) NOT NULL,
   PRIMARY KEY (`field_id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `fields`
---
-
-LOCK TABLES `fields` WRITE;
-/*!40000 ALTER TABLE `fields` DISABLE KEYS */;
-INSERT INTO `fields` VALUES (1,'Textfield');
-/*!40000 ALTER TABLE `fields` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `projects`
@@ -213,18 +156,8 @@ CREATE TABLE `projects` (
   PRIMARY KEY (`project_id`),
   KEY `FK_projects_company` (`company_id`),
   CONSTRAINT `FK_projects_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `projects`
---
-
-LOCK TABLES `projects` WRITE;
-/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (32,'Demo1',1,'asdasdsad','asdadsasdxx',10,123123,'2015-09-01','2015-10-01',NULL);
-/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `staff`
@@ -248,18 +181,8 @@ CREATE TABLE `staff` (
   PRIMARY KEY (`staff_id`),
   KEY `FK_staff_company` (`company_id`),
   CONSTRAINT `FK_staff_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=154 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `staff`
---
-
-LOCK TABLES `staff` WRITE;
-/*!40000 ALTER TABLE `staff` DISABLE KEYS */;
-INSERT INTO `staff` VALUES (72,'','','','','',NULL,NULL,NULL,10,0),(73,'','','','','',NULL,NULL,NULL,10,0),(74,'','Vic','Castillo','Cebedo','II','Test','cebedo.vii@gmail.com','9226110429',10,500),(75,'','Smith','Johnson','Williams','','Test','cebedo.vii@gmail.com','9226110429',10,400),(76,'','Williams','Smith','Johnson','','Test','cebedo.vii@gmail.com','9226110429',10,300),(77,'','Johnson','Smith','Williams','','Test','cebedo.vii@gmail.com','9226110429',10,350),(78,'Engr.','Bob','Marley','Lannister','','Test','cebedo.vii@gmail.com','9226110429',10,350),(79,'Dr.','Erlang','Churva','Looo','III','Test','cebedo.vii@gmail.com','9226110429',10,350);
-/*!40000 ALTER TABLE `staff` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `system_configuration`
@@ -277,18 +200,8 @@ CREATE TABLE `system_configuration` (
   UNIQUE KEY `name` (`name`),
   KEY `FK_system_configuration_company` (`company_id`),
   CONSTRAINT `FK_system_configuration_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `system_configuration`
---
-
-LOCK TABLES `system_configuration` WRITE;
-/*!40000 ALTER TABLE `system_configuration` DISABLE KEYS */;
-INSERT INTO `system_configuration` VALUES (2,'ROOT_INIT','1',NULL);
-/*!40000 ALTER TABLE `system_configuration` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `system_users`
@@ -311,18 +224,8 @@ CREATE TABLE `system_users` (
   KEY `FK_system_users_staff` (`staff_id`),
   CONSTRAINT `FK_system_users_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`),
   CONSTRAINT `FK_system_users_staff` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=229 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `system_users`
---
-
-LOCK TABLES `system_users` WRITE;
-/*!40000 ALTER TABLE `system_users` DISABLE KEYS */;
-INSERT INTO `system_users` VALUES (219,'root_098','4ca3c78a6db368685a6a1a67045de747',1,NULL,NULL,0,0),(227,'demo_admin1','54044c4ed6c192034e51acfaaf48d948',0,10,72,1,0),(228,'standard_user1','1f27cd46ff32ac1069d0410ddfeca481',0,10,73,0,0);
-/*!40000 ALTER TABLE `system_users` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `tasks`
@@ -347,18 +250,8 @@ CREATE TABLE `tasks` (
   KEY `FK_tasks_company` (`company_id`),
   CONSTRAINT `FK_tasks_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`),
   CONSTRAINT `FK_tasks_project` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tasks`
---
-
-LOCK TABLES `tasks` WRITE;
-/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES (84,'Task 1','Test 4','2015-01-10','2015-01-10',3,5,32,2,10),(85,'Task 2','','2015-01-13','2015-01-15',2,3,32,2,10),(86,'Task 3','Test 6','2015-01-15','2015-01-18',6,5,32,2,10),(87,'Task 4','Test 7','2015-01-21',NULL,10,0,32,1,10),(88,'Task 5','Test 8','2015-01-31','2015-01-31',3,4,32,2,10),(89,'Task 5','Test 8','2015-02-03','2015-02-04',3,5,32,2,10);
-/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -369,4 +262,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-30 11:53:05
+-- Dump completed on 2015-11-16  9:38:18
--- mod-webapp-db/2015.11.16.Yog.sql
+++ mod-webapp-db/2015.11.16.Yog.sql
@@ -31,10 +31,6 @@ CREATE TABLE `assignments_project_field` (
   CONSTRAINT `FK_project_field` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `assignments_project_field` */
-
-insert  into `assignments_project_field`(`project_id`,`field_id`,`label`,`value`) values (32,1,'qweqweqwe2222vvvqq','qweqweqwewqeqwe');
-
 /*Table structure for table `assignments_project_staff` */
 
 DROP TABLE IF EXISTS `assignments_project_staff`;
@@ -48,10 +44,6 @@ CREATE TABLE `assignments_project_staff` (
   CONSTRAINT `FK_assignments_project_staff_stf` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `assignments_project_staff` */
-
-insert  into `assignments_project_staff`(`project_id`,`staff_id`) values (32,74),(32,75),(32,77),(32,78),(32,79);
-
 /*Table structure for table `assignments_task_staff` */
 
 DROP TABLE IF EXISTS `assignments_task_staff`;
@@ -64,8 +56,6 @@ CREATE TABLE `assignments_task_staff` (
   CONSTRAINT `FK_assignments_task_staff` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`),
   CONSTRAINT `FK_tasks_task` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Data for the table `assignments_task_staff` */
 
 /*Table structure for table `audit_logs` */
 
@@ -91,13 +81,8 @@ CREATE TABLE `audit_logs` (
   KEY `FK_audit_logs_project_id` (`project_id`),
   KEY `FK_audit_logs_company_id` (`company_id`),
   CONSTRAINT `FK_audit_logs_company_id` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`),
-  CONSTRAINT `FK_audit_logs_project_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`),
-  CONSTRAINT `FK_audit_logs_users` FOREIGN KEY (`user_id`) REFERENCES `system_users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
-
-/*Data for the table `audit_logs` */
-
-insert  into `audit_logs`(`auditlog_id`,`date_executed`,`ip_address`,`user_id`,`company_id`,`project_id`,`action`,`object_name`,`object_id`,`object_key`,`assoc_object_name`,`assoc_object_id`,`assoc_object_key`,`entry_name`) values (1,'2015-09-30 11:49:04','127.0.0.1',228,10,32,120,'project',32,NULL,'staff',NULL,'All','All'),(2,'2015-09-30 11:49:17','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',75,NULL,' Smith Johnson Williams '),(3,'2015-09-30 11:49:17','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',76,NULL,' Williams Smith Johnson '),(4,'2015-09-30 11:49:17','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',77,NULL,' Johnson Smith Williams '),(5,'2015-09-30 11:49:31','127.0.0.1',228,10,32,101,'project',32,NULL,'staff',NULL,'Mass','Mass'),(6,'2015-09-30 11:49:31','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',76,NULL,' Williams Smith Johnson '),(7,'2015-09-30 11:49:31','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',77,NULL,' Johnson Smith Williams '),(8,'2015-09-30 11:49:31','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',78,NULL,'Engr. Bob Marley Lannister '),(9,'2015-09-30 11:49:32','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',79,NULL,'Dr. Erlang Churva Looo III'),(10,'2015-09-30 11:49:32','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',74,NULL,' Vic Castillo Cebedo II'),(11,'2015-09-30 11:49:32','127.0.0.1',228,10,32,116,'project',32,NULL,'staff',75,NULL,' Smith Johnson Williams '),(12,'2015-09-30 11:49:52','127.0.0.1',228,10,32,119,'project',32,NULL,'staff',76,NULL,' Williams Smith Johnson ');
+  CONSTRAINT `FK_audit_logs_project_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `companies` */
 
@@ -110,12 +95,9 @@ CREATE TABLE `companies` (
   `date_started` datetime NOT NULL,
   `date_expiration` datetime NOT NULL,
   `beta_tester` tinyint(4) NOT NULL DEFAULT '0',
+  `theme` varchar(32) NOT NULL,
   PRIMARY KEY (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-
-/*Data for the table `companies` */
-
-insert  into `companies`(`company_id`,`name`,`description`,`date_started`,`date_expiration`,`beta_tester`) values (10,'Demo 1','','2015-09-01 00:00:00','2016-01-07 00:00:00',1);
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `fields` */
 
@@ -126,11 +108,7 @@ CREATE TABLE `fields` (
   `name` varchar(32) NOT NULL,
   PRIMARY KEY (`field_id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
-/*Data for the table `fields` */
-
-insert  into `fields`(`field_id`,`name`) values (1,'Textfield');
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `projects` */
 
@@ -150,11 +128,7 @@ CREATE TABLE `projects` (
   PRIMARY KEY (`project_id`),
   KEY `FK_projects_company` (`company_id`),
   CONSTRAINT `FK_projects_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
-
-/*Data for the table `projects` */
-
-insert  into `projects`(`project_id`,`name`,`status`,`location`,`notes`,`company_id`,`physical_target`,`date_start`,`date_completion_target`,`date_completion_actual`) values (32,'Demo1',1,'asdasdsad','asdadsasdxx',10,123123,'2015-09-01','2015-10-01',NULL);
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `staff` */
 
@@ -175,11 +149,7 @@ CREATE TABLE `staff` (
   PRIMARY KEY (`staff_id`),
   KEY `FK_staff_company` (`company_id`),
   CONSTRAINT `FK_staff_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=latin1;
-
-/*Data for the table `staff` */
-
-insert  into `staff`(`staff_id`,`name_prefix`,`name_first`,`name_middle`,`name_last`,`name_suffix`,`position`,`email`,`contact_number`,`company_id`,`wage`) values (72,'','','','','',NULL,NULL,NULL,10,0),(73,'','','','','',NULL,NULL,NULL,10,0),(74,'','Vic','Castillo','Cebedo','II','Test','cebedo.vii@gmail.com','9226110429',10,500),(75,'','Smith','Johnson','Williams','','Test','cebedo.vii@gmail.com','9226110429',10,400),(76,'','Williams','Smith','Johnson','','Test','cebedo.vii@gmail.com','9226110429',10,300),(77,'','Johnson','Smith','Williams','','Test','cebedo.vii@gmail.com','9226110429',10,350),(78,'Engr.','Bob','Marley','Lannister','','Test','cebedo.vii@gmail.com','9226110429',10,350),(79,'Dr.','Erlang','Churva','Looo','III','Test','cebedo.vii@gmail.com','9226110429',10,350);
+) ENGINE=InnoDB AUTO_INCREMENT=154 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `system_configuration` */
 
@@ -194,11 +164,7 @@ CREATE TABLE `system_configuration` (
   UNIQUE KEY `name` (`name`),
   KEY `FK_system_configuration_company` (`company_id`),
   CONSTRAINT `FK_system_configuration_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
-
-/*Data for the table `system_configuration` */
-
-insert  into `system_configuration`(`sysconf_id`,`name`,`value`,`company_id`) values (2,'ROOT_INIT','1',NULL);
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 /*Table structure for table `system_users` */
 
@@ -218,11 +184,7 @@ CREATE TABLE `system_users` (
   KEY `FK_system_users_staff` (`staff_id`),
   CONSTRAINT `FK_system_users_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`),
   CONSTRAINT `FK_system_users_staff` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=229 DEFAULT CHARSET=latin1;
-
-/*Data for the table `system_users` */
-
-insert  into `system_users`(`user_id`,`username`,`password`,`super_admin`,`company_id`,`staff_id`,`company_admin`,`login_attempts`) values (219,'root_098','4ca3c78a6db368685a6a1a67045de747',1,NULL,NULL,0,0),(227,'demo_admin1','54044c4ed6c192034e51acfaaf48d948',0,10,72,1,0),(228,'standard_user1','1f27cd46ff32ac1069d0410ddfeca481',0,10,73,0,0);
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `tasks` */
 
@@ -244,11 +206,7 @@ CREATE TABLE `tasks` (
   KEY `FK_tasks_company` (`company_id`),
   CONSTRAINT `FK_tasks_company` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`),
   CONSTRAINT `FK_tasks_project` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=latin1;
-
-/*Data for the table `tasks` */
-
-insert  into `tasks`(`task_id`,`title`,`content`,`date_start`,`actual_date_start`,`duration`,`actual_duration`,`project_id`,`status`,`company_id`) values (84,'Task 1','Test 4','2015-01-10','2015-01-10',3,5,32,2,10),(85,'Task 2','','2015-01-13','2015-01-15',2,3,32,2,10),(86,'Task 3','Test 6','2015-01-15','2015-01-18',6,5,32,2,10),(87,'Task 4','Test 7','2015-01-21',NULL,10,0,32,1,10),(88,'Task 5','Test 8','2015-01-31','2015-01-31',3,4,32,2,10),(89,'Task 5','Test 8','2015-02-03','2015-02-04',3,5,32,2,10);
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 /* Procedure structure for procedure `test_multi_sets` */
 
