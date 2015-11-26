2bd368474f42739ea9752f39a9fa8b3a270583ff
VicCebedo <cebedo.vii@gmail.com>
2015-11-26 15:45:11 +0800

    Improved the performance logging. Logging all Controllers.

--- mod-webapp-codebase/src/main/java/com/cebedo/pmsys/constants/RegistryLogger.java
+++ mod-webapp-codebase/src/main/java/com/cebedo/pmsys/constants/RegistryLogger.java
@@ -6,4 +6,6 @@ public class RegistryLogger {
 
     public static final String LOGGER_TAIL = "webappTailLogger";
 
+    public static final String LOGGER_PERFORMANCE = "webappPerformanceLogger";
+
 }
